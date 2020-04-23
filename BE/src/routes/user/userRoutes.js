8712bad9cb2c09277b8dda953954f6bc7a624bfe
Jeff Lapp <lappjeff20@gmail.com>
2020-04-23 17:28:27 -0400

    created get user route

--- BE/src/routes/user/userRoutes.js
+++ BE/src/routes/user/userRoutes.js
@@ -3,11 +3,24 @@ const router = require("express").Router();
 const {
 	addChannelToUser,
 	removeChannelFromUser,
+	getUser,
 } = require("../../service/usersService");
 
 const { isAuthenticated } = require("../../helpers/utils.js");
 
 const { handleError } = require("../../helpers/errorHelpers");
+
+router.get("", isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.user._id;
+
+		const user = await getUser(userId);
+
+		return res.status(200).json(user);
+	} catch (err) {
+		handleError(err, res);
+	}
+});
 
 router.put("/addChannel/:channelId", isAuthenticated, async (req, res) => {
 	try {
@@ -16,9 +29,7 @@ router.put("/addChannel/:channelId", isAuthenticated, async (req, res) => {
 
 		await addChannelToUser(channelId, userId);
 
-		return res
-			.status(200)
-			.json({ message: "Channel added!", status: "success" });
+		return res.status(200).json({ message: "Channel added!" });
 	} catch (err) {
 		handleError(err, res);
 	}
