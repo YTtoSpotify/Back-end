95d8521a6a7b0519e31cdfb441fbaf5e053c49c7
Jeff Lapp <lappjeff20@gmail.com>
2020-05-05 11:28:10 -0400

    added latestUploadId property to channels to keep track of most recent upload

--- BE/src/db/models/channelModel.js
+++ BE/src/db/models/channelModel.js
@@ -5,6 +5,7 @@ const channelSchema = mongoose.Schema({
 	img: { type: String, required: true },
 	url: { type: String, required: true },
 	ytId: { type: String, required: true },
+	latestUploadId: { type: String, default: "" },
 });
 
 const Channel = mongoose.model("Channel", channelSchema);
